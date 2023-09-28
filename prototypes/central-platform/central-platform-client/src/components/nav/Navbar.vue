5d9b9507bc19a68568b77d8f303a2d08929ea866
AProgramFailure <synthw8@gmail.com>
2023-09-28 10:55:56 +0200

    updated frontend for central client

--- prototypes/central-platform/central-platform-client/src/components/nav/Navbar.vue
+++ prototypes/central-platform/central-platform-client/src/components/nav/Navbar.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 
+    const route = useRoute();
+
 </script>
 
 
 <template>
     <div class="w-full h-12 flex flex-row items-center justify-center bg-emerald-500">
-        <h1 class="text-white font-semibold text-[16px]">DASHBOARD</h1>
+        <h1 class="text-white font-semibold text-[16px]">DASHBOARD - {{ route.name?.toString().toUpperCase() }}</h1>
     </div>
 </template>
