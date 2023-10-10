5d9ce7c9f62ab13eecb9639f9b701be32247099a
Daniel-Lizarazo-Fuentes <daniel.lizarazo.fuentes@antipy.com>
2023-10-10 23:54:18 +0200

    Central: marker colors, new dependency

--- prototypes/central-platform/central-platform-client/src/pages/home.vue
+++ prototypes/central-platform/central-platform-client/src/pages/home.vue
@@ -2,7 +2,9 @@
 import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
 import { PointExpression } from "leaflet";
+import L from "leaflet";
 import { useOrganizationStore } from "~/stores/Organization/OrganizationStore";
+import { Inventory } from "~/types/Inventory/Inventory";
 
 const organizationStore = useOrganizationStore();
 const { getOrganizations } = organizationStore;
@@ -51,25 +53,32 @@ const mapLocations = computed(() => {
 let zoom = ref(7);
 let center = ref<PointExpression>([52.1926, 5.2913]);
 
-const markerStyles = computed(() => {
-  return mapLocations.value.map(location => {
-    const totalInventory = location.inventory.reduce((acc, item) => acc + item.quantity, 0);
+const calculateTotalInventory = (inventory: Inventory[]) => {
+  return inventory.reduce((total, item) => total + item.quantity, 0);
+};
 
-    let colorClass = '';
-    if (totalInventory >= 0 && totalInventory <= 50) {
-      colorClass = 'low-inventory';
-    } else if (totalInventory >= 51 && totalInventory <= 99) {
-      colorClass = 'medium-inventory';
-    } else if (totalInventory >= 100 && totalInventory <= 150) {
-      colorClass = 'high-inventory';
-    }
+function createMarkerIcon(totalQuantity: number) {
+  let color;
 
-    return {
-      totalInventory,
-      colorClass,
-    };
+  if (totalQuantity < 50) {
+    color = "green";
+  } else if (totalQuantity >= 50 && totalQuantity < 100) {
+    color = "yellow";
+  } else if (totalQuantity >= 100 && totalQuantity < 150) {
+    color = "orange";
+  } else {
+    color = "red";
+  }
+
+  return L.divIcon({
+    className: "custom-marker-icon",
+    html: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="32" viewBox="0 0 24 32">
+            <path d="M12 2C6.478 2 2 6.478 2 12c0 8 10 18 10 18s10-10 10-18c0-5.522-4.478-10-10-10zm0 18c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z" fill="${color}" stroke="white" stroke-width="2" />
+    </svg>`,
+    iconSize: [24, 32],
+    iconAnchor: [18, 36],
   });
-});
+}
 </script>
 
 <template>
@@ -128,6 +137,7 @@ const markerStyles = computed(() => {
           v-for="(location, locationIndex) in mapLocations"
           :key="locationIndex"
           :lat-lng="[parseFloat(location.lat), parseFloat(location.lon)]"
+          :icon="createMarkerIcon(calculateTotalInventory(location.inventory))"
         >
           <l-popup>
             <div>
@@ -135,7 +145,7 @@ const markerStyles = computed(() => {
             </div>
             <div>{{ location.address }}</div>
             <div>
-              Total Inventory: {{ markerStyles[locationIndex].totalInventory }}
+              Total Inventory: {{ calculateTotalInventory(location.inventory) }}
             </div>
           </l-popup>
         </l-marker>
