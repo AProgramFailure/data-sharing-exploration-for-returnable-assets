a44411fc4a2c72639abaaf5ea9c33cf35699cfcf
AProgramFailure <synthw8@gmail.com>
2023-10-11 15:00:39 +0200

    added node and inventory ednpoints

--- prototypes/federated-data-sharing/federated-data-sharing-node/server/trpc/routers/Inventory/inventory.ts
+++ prototypes/federated-data-sharing/federated-data-sharing-node/server/trpc/routers/Inventory/inventory.ts
@@ -1,6 +1,21 @@
 import { z } from "zod";
 import { publicProcedure, router } from "../../trpc";
 
+type InventoryResponse = {
+    message: string,
+    success: boolean,
+    payload: Inventory[] | null
+}
+
+type Inventory = {
+    id?: number,
+    inventory_name: string,
+    item_type: string,
+    quantity: number,
+    location_id: number,
+    security : "private" | "public" | "subscribe",
+}
+
 export const inventoryRouter = router({
     getInventory: publicProcedure
     .query( ({ ctx }) => {
@@ -10,6 +25,67 @@ export const inventoryRouter = router({
         const inventories = fetchInventory.all()
         return {
             inventories
+        }
+    }),
+    addInventory: publicProcedure
+    .input(
+        z.object({
+            item_type : z.string(),
+            inventory_name : z.string(),
+            quantity: z.number(),
+            location_id: z.number(),
+            security: z.literal("private").or(z.literal("subscribe")).or(z.literal("public"))
+        })
+    )
+    .query( ({ input, ctx }) => {
+        let response : InventoryResponse = {
+            message: "error occured before db transaction",
+            success: false,
+            payload: null
+        }
+        const fetchLocation = ctx.db.prepare(`
+        SELECT * FROM location WHERE location_id = ${input.location_id}
+        `)
+
+        const fetched_inventory = fetchLocation.all();
+
+        if(fetched_inventory){
+            const newInvenotry : Inventory = {
+                item_type: input.item_type,
+                inventory_name: input.inventory_name,
+                quantity: input.quantity,
+                location_id: input.location_id,
+                security: input.security
+            }
+
+            const insertInventory = ctx.db.prepare<Inventory>(`
+                INSERT INTO invenotry (item_type, inventory_name, quantity, security) VALUES (:item_type, :inventory_name, :quantity, :security )
+            `)
+
+            insertInventory.run(newInvenotry)
+
+            const getInsertedInventory = ctx.db.prepare(`
+            SELECT * FROM inventory WHERE inventory_name = ${newInvenotry.inventory_name}
+            `)
+
+            const inventory = getInsertedInventory.all()
+
+            response = {
+                message: "Successfully Added an Invenotry",
+                success: true,
+                payload: [] as Inventory[]
+            }
+        }
+        else {
+            response = {
+                message: "location not found",
+                success: false,
+                payload: null
+            }
+        }
+
+        return {
+            response
         }
     })
 })
