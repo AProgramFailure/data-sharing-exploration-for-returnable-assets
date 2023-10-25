0ec6a22b018301bf5b526861622a12d503da5cd2
Daniel-Lizarazo-Fuentes <daniel.lizarazo.fuentes@antipy.com>
2023-10-25 15:10:15 +0200

    Centralized-Server: fixed lidl/kordaat locations being in the sea

--- prototypes/central-platform/central-platform-server/src/main/resources/data.sql
+++ prototypes/central-platform/central-platform-server/src/main/resources/data.sql
@@ -27,8 +27,8 @@ VALUES
     ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a85a', '1b92a522-e99d-42e7-925d-7a065d6c9d50', 'Hertog Jan Brewery 2', 'Hertog Jan Brewery Address 2', '51.378670', '5.440262'),
 
     -- Brewery 3: Kordaat
-    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a85b', 'eec8b7a2-20a2-47da-8c9a-bac8d68a3f5a', 'Kordaat Brewery 1', 'Kordaat Brewery Address 1', '52.155722', '4.202924'),
-    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a85c', 'eec8b7a2-20a2-47da-8c9a-bac8d68a3f5a', 'Kordaat Brewery 2', 'Kordaat Brewery Address 2', '52.303477', '5.287159'),
+    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a85b', 'eec8b7a2-20a2-47da-8c9a-bac8d68a3f5a', 'Kordaat Brewery 1', 'Kordaat Brewery Address 1', '51.560131', '4.874955'),
+    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a85c', 'eec8b7a2-20a2-47da-8c9a-bac8d68a3f5a', 'Kordaat Brewery 2', 'Kordaat Brewery Address 2', '52.271514', '4.503863'),
 
     -- Brewery 4: Grolsch
     ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a85d', '7c697a6f-8d4a-4c8b-8e21-7e57fc07316a', 'Grolsch Brewery 1', 'Grolsch Brewery Address 1', '52.435474', '6.661176'),
@@ -38,11 +38,11 @@ VALUES
 INSERT INTO location (id, organization_id, name, address, latitude, longitude)
 VALUES
     -- Supermarket 1: Lidl
-    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a85f', '3e4bdc4e-51d2-4e82-a05a-93ac3e3a7861', 'Lidl Supermarket 1', 'Lidl Supermarket Address 1', '51.719439', '4.277911'),
-    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a860', '3e4bdc4e-51d2-4e82-a05a-93ac3e3a7861', 'Lidl Supermarket 2', 'Lidl Supermarket Address 2', '52.193168', '4.171490'),
-    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a861', '3e4bdc4e-51d2-4e82-a05a-93ac3e3a7861', 'Lidl Supermarket 3', 'Lidl Supermarket Address 3', '52.462900', '4.056246'),
+    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a85f', '3e4bdc4e-51d2-4e82-a05a-93ac3e3a7861', 'Lidl Supermarket 1', 'Lidl Supermarket Address 1', '51.719439', '5.277911'),
+    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a860', '3e4bdc4e-51d2-4e82-a05a-93ac3e3a7861', 'Lidl Supermarket 2', 'Lidl Supermarket Address 2', '52.193168', '5.171490'),
+    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a861', '3e4bdc4e-51d2-4e82-a05a-93ac3e3a7861', 'Lidl Supermarket 3', 'Lidl Supermarket Address 3', '51.462900', '5.956246'),
     ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a862', '3e4bdc4e-51d2-4e82-a05a-93ac3e3a7861', 'Lidl Supermarket 4', 'Lidl Supermarket Address 4', '52.626750', '5.278769'),
-    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a863', '3e4bdc4e-51d2-4e82-a05a-93ac3e3a7861', 'Lidl Supermarket 5', 'Lidl Supermarket Address 5', '52.477077', '4.192023'),
+    ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a863', '3e4bdc4e-51d2-4e82-a05a-93ac3e3a7861', 'Lidl Supermarket 5', 'Lidl Supermarket Address 5', '51.477077', '5.192023'),
 
     -- Supermarket 2: Aldi
     ('eaa663d1-4eb7-4a9f-9a90-d61c0d82a864', '9c24c37b-6a5b-4c5a-8b4e-0b3a5a8f385d', 'Aldi Supermarket 1', 'Aldi Supermarket Address 1', '51.560131', '4.874955'),
