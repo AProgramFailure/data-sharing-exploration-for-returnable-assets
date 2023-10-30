8c76f2ed0eba36344929424ede769ec8bceeb53d
Daniel-Lizarazo-Fuentes <daniel.lizarazo.fuentes@antipy.com>
2023-10-30 21:04:35 +0100

    Central: Updating requests

--- prototypes/central-platform/central-platform-server/src/main/java/com/centralplatform/server/controller/User/Admin/AdminController.java
+++ prototypes/central-platform/central-platform-server/src/main/java/com/centralplatform/server/controller/User/Admin/AdminController.java
@@ -1,7 +1,9 @@
 package com.centralplatform.server.controller.User.Admin;
 
 import com.centralplatform.server.model.UserOrganizationApplication.UserOrganizationApplication;
+import com.centralplatform.server.payload.request.UserOrganizationApplication.UserOrganizationApplicationCreateRequest;
 import com.centralplatform.server.payload.request.UserOrganizationApplication.UserOrganizationApplicationRequest;
+import com.centralplatform.server.payload.request.UserOrganizationApplication.UserOrganizationApplicationUpdateRequest;
 import com.centralplatform.server.service.UserOrganizationApplication.UserOrganizationApplicationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -22,7 +24,12 @@ public class AdminController {
     @RequestMapping(value = "/user-assign-request", method = RequestMethod.POST)
     @PreAuthorize("hasAuthority('admin:read')")
     public ResponseEntity<? extends List<UserOrganizationApplication>> getUserOrganizationApplications() {
-
         return ResponseEntity.ok(userOrganizationApplicationService.getUserOrganizationApplications());
+    }
+
+    @RequestMapping(value = "/user-assign-request/update/{id}", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('admin:update')")
+    public ResponseEntity<UserOrganizationApplication> updateUserOrganizationApplication(@RequestBody UserOrganizationApplicationUpdateRequest request, @PathVariable("id") String id) {
+        return ResponseEntity.ok(userOrganizationApplicationService.updateUserOrganizationApplication(request, id));
     }
 }
--- prototypes/central-platform/central-platform-server/src/main/java/com/centralplatform/server/service/UserOrganizationApplication/UserOrganizationApplicationService.java
+++ prototypes/central-platform/central-platform-server/src/main/java/com/centralplatform/server/service/UserOrganizationApplication/UserOrganizationApplicationService.java
@@ -8,6 +8,7 @@ import com.centralplatform.server.model.UserOrganizationApplication.UserOrganiza
 import com.centralplatform.server.payload.request.Organization.OrganizationRequest;
 import com.centralplatform.server.payload.request.UserOrganizationApplication.UserOrganizationApplicationCreateRequest;
 import com.centralplatform.server.payload.request.UserOrganizationApplication.UserOrganizationApplicationRequest;
+import com.centralplatform.server.payload.request.UserOrganizationApplication.UserOrganizationApplicationUpdateRequest;
 import com.centralplatform.server.repository.User.UserRepository;
 import com.centralplatform.server.repository.UserOrganizationApplicationRepository.UserOrganizationApplicationRepository;
 import lombok.RequiredArgsConstructor;
@@ -66,6 +67,11 @@ public class UserOrganizationApplicationService {
 
 
 
+    public UserOrganizationApplication updateUserOrganizationApplication(UserOrganizationApplicationUpdateRequest request, String id) {
+        Optional<UserOrganizationApplication> userOrganizationApplication = organizationApplicationRepository.findById(UUID.fromString(id));
+        userOrganizationApplication.ifPresent(organizationApplication -> organizationApplication.setStatus(request.getUserOrganizationApplication().getStatus()));
+        return null;
+    }
 //    public void deleteUserOrganizationApplicationById(String id) {
 //        organizationRequestRepository.delete(organizationRequestRepository.findById(UUID.fromString(id)).orElseThrow());
 //    }
