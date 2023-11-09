c048ab83b0607eeddf3fad5aa4ac68eef6aa2783
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2023-11-09 16:15:51 +0100

    Central: organizations now filtered by orgAccesses

--- prototypes/central-platform/central-platform-server/src/main/java/com/centralplatform/server/service/Organization/OrganizationServiceImpl.java
+++ prototypes/central-platform/central-platform-server/src/main/java/com/centralplatform/server/service/Organization/OrganizationServiceImpl.java
@@ -3,13 +3,21 @@ package com.centralplatform.server.service.Organization;
 import com.centralplatform.server.dto.Organization.OrganizationDTO;
 import com.centralplatform.server.dto.Organization.OrganizationDTOConverter;
 import com.centralplatform.server.model.Organization.Organization;
+import com.centralplatform.server.model.OrganizationAccess.OrganizationAccess;
+import com.centralplatform.server.model.User.User;
 import com.centralplatform.server.payload.request.Organization.OrganizationRequest;
 import com.centralplatform.server.repository.Organization.OrganizationRepository;
+import com.centralplatform.server.repository.OrganizationAccess.OrganizationAccessRepository;
+import com.centralplatform.server.repository.User.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -18,10 +26,30 @@ public class OrganizationServiceImpl implements OrganizationService {
 
     private final OrganizationRepository organizationRepository;
     private final OrganizationDTOConverter converter;
+    private final OrganizationAccessRepository organizationAccessRepository;
+    private final UserRepository userRepository;
 
     @Override
     public List<Organization> getOrganizations(OrganizationRequest request) {
-        return organizationRepository.findAll();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<Organization> resultList = new ArrayList<>();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            String userEmail = ((UserDetails) authentication.getPrincipal()).getUsername();
+            Optional<User> user = ((userRepository.findByEmail(userEmail)));
+
+            if(user.isPresent()){
+                OrganizationAccess organizationAccess = organizationAccessRepository.findOrganizationAccessByOrganizationId(UUID.fromString(user.get().getOrganizationId()));
+
+                for (Organization org : organizationRepository.findAll()) {
+                    if (org.getId().equals(organizationAccess.getOrganization().getId()) || organizationAccess.getAllowedOrganizations().contains(org)) {
+                        resultList.add(org);
+                    }
+                }
+            }
+        }
+
+        return resultList;
     }
 
     @Override
